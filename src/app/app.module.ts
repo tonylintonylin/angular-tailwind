51f9da6e48ef411a04a64cf917ffb20603d89dcb
Tony Lin <tonylinbusiness@gmail.com>
2020-12-13 10:51:34 -0500

    Added two column

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
   ],
   imports: [
     BrowserModule,
